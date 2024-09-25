436eb547d6bfdfbf9c256f4627f99b558f597b99
soundaryak0014 <146517227+soundaryak0014@users.noreply.github.com>
2024-09-25 11:42:14 +0530

    Add files via upload

---
+++ Celsius to Fahrenheit.py
@@ -0,0 +1,12 @@
+#CELSIUS TO FAHRENHEIT
+
+Celsius= int(input("Enter the Value:"))
+Fahrenheit=(Celsius-9/5)+32
+print(f"The Fahrenheit value is:{Fahrenheit:.2f}")
+
+
+#FAHRENHEIT TO CELSIUS
+
+Fahrenheit = int(input("Enter the Value:"))
+Celsius=(Fahrenheit-32)*(5/9)
+print(f"The celsius value is:{Celsius:.2f}")
