5bbd6dc565c460464e1ec6a4aed8d646e5db7161
soundaryak0014 <146517227+soundaryak0014@users.noreply.github.com>
2024-09-25 11:19:29 +0530

    Add files via upload

---
+++ kilometers to miles.py
@@ -0,0 +1,11 @@
+#KILOMETERS TO MILES:
+
+KM=int(input("Enter the kilometers:"))
+miles=KM*0.621371
+print("The miles are:", format(miles,".2f"))
+
+#MILES TO KILOMETERS:
+
+miles=int(input("Enter the miles:"))
+KM=miles * 1.60934
+print("The Kilometers are:",format(KM,".2f"))
