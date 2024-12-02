1182dc0080da8e2ae340131c7b6089ed9c374dba
soundaryak0014 <146517227+soundaryak0014@users.noreply.github.com>
2024-12-02 14:57:21 +0530

    Add files via upload

---
+++ Remove First and Last Character.py
@@ -0,0 +1,6 @@
+# To remove the first and last elements from a given string.
+
+original_string = input("Enter the string value:")
+result = original_string[1:-1]
+print("Original String:", original_string)
+print("Modified String:", result)
